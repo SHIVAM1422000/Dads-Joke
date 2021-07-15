3bd28c5c4a50cc4e3d9de5564f08a10079f74da0
SHIVAM1422000 <shivamojha1422000@gmail.com>
2021-07-15 19:34:20 +0530

    First Commit

---
+++ src/Joke.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import "./Joke.css";
+class Joke extends Component{
+
+constructor(props){
+    super(props);
+    this.handleUpvote=this.handleUpvote.bind(this);
+    this.handleDownvote=this.handleDownvote.bind(this);
+}
+
+handleDownvote(){
+    var id=this.props.id;
+    var value=this.props.downvalue;
+    this.props.downvote(id,value);
+}
+
+handleUpvote(){ 
+    var id=this.props.id;
+    var value=this.props.upvalue;
+    this.props.upvote(id,value);
+}
+
+
+render(){
+
+return(
+
+<div className="Joke">
+<div className="Joke-button">
+    <i className="fa fa-arrow-up" onClick={this.handleUpvote}/>
+    <span className="Joke-votes"> {this.props.votes}</span>
+    <i className="fa fa-arrow-down" onClick={this.handleDownvote}/>
+</div>
+<div className="Joke-text">{this.props.joke}</div>
+</div>
+
+
+);
+
+}
+
+}
+
+
+export default Joke;
