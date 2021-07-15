4676b5f35ae24be065ba4b309ad5e587df5e893c
SHIVAM1422000 <shivamojha1422000@gmail.com>
2021-07-15 20:23:30 +0530

    Added Dyanmic color and Dynamic Emoji

--- src/Joke.js
+++ src/Joke.js
@@ -2,6 +2,42 @@ import React, { Component } from 'react';
 import "./Joke.css";
 class Joke extends Component{
 
+    getColor() {
+        if (this.props.votes >= 15) {
+          return "#4CAF50";
+        } else if (this.props.votes >= 12) {
+          return "#8BC34A";
+        } else if (this.props.votes >= 9) {
+          return "#CDDC39";
+        } else if (this.props.votes >= 6) {
+          return "#FFEB3B";
+        } else if (this.props.votes >= 3) {
+          return "#FFC107";
+        } else if (this.props.votes >= 0) {
+          return "#FF9800";
+        } else {
+          return "#f44336";
+        }
+      }
+      getEmoji() {
+        if (this.props.votes >= 15) {
+          return "em em-rolling_on_the_floor_laughing";
+        } else if (this.props.votes >= 12) {
+          return "em em-laughing";
+        } else if (this.props.votes >= 9) {
+          return "em em-smiley";
+        } else if (this.props.votes >= 6) {
+          return "em em-slightly_smiling_face";
+        } else if (this.props.votes >= 3) {
+          return "em em-neutral_face";
+        } else if (this.props.votes >= 0) {
+          return "em em-confused";
+        } else {
+          return "em em-angry";
+        }
+      }
+
+
 constructor(props){
     super(props);
     this.handleUpvote=this.handleUpvote.bind(this);
@@ -28,12 +64,12 @@ return(
 <div className="Joke">
 <div className="Joke-button">
     <i className="fa fa-arrow-up" onClick={this.handleUpvote}/>
-    <span className="Joke-votes"> {this.props.votes}</span>
+    <span className="Joke-votes" style={{borderColor:this.getColor()}}> {this.props.votes}</span>
     <i className="fa fa-arrow-down" onClick={this.handleDownvote}/>
 </div>
 <div className="Joke-text">{this.props.joke}</div>
   <div className="Joke-smiley">
-  <i class="em em-rolling_on_the_floor_laughing" aria-role="presentation" aria-label="ROLLING ON THE FLOOR LAUGHING"></i>
+  <i class={this.getEmoji()}></i>
    </div>
 </div>
 
